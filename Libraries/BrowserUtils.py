44f2fd40408937f9e18685032e5c41d88020b24c
Ville Salonen <ville.salonen@tietoevry.com>
2022-01-17 13:02:09 +0200

    added super_secret_source
    test works and libspec can be created from command line but VS Code extension does not work

--- Libraries/BrowserUtils.py
+++ Libraries/BrowserUtils.py
@@ -1,10 +1,12 @@
 from robot.api.deco import keyword, library
 from robot.libraries.BuiltIn import BuiltIn
 
+from Libraries.SomeOtherUtils import super_secret_source
+
 @library
 class BrowserUtils:
     """ Browser Utils """
     @keyword
     def some_browser_util(self):
         """ Foobar some documentation """
-        BuiltIn().log("called some_browser_util")
+        BuiltIn().log_to_console("read secret: " + super_secret_source())
---
+++ Libraries/SomeOtherUtils.py
@@ -0,0 +1,2 @@
+def super_secret_source():
+    return "very secret stuff"
